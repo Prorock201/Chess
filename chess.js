4e31069c3d0251c878ffb1ecb331384d2e74657c
Prorock201 <prorock@list.ru>
2014-04-10 17:24:40 +0300

    2. "Add Objects"

--- chess.js
+++ chess.js
@@ -3,9 +3,42 @@
  */
 window.onload = bindEventHandlers;
 
-var Board = {}
+var Files = {
+    File_A: 'A',
+    File_B: 'B',
+    File_C: 'C',
+    File_D: 'D',
+    File_E: 'E',
+    File_F: 'F',
+    File_G: 'G',
+    File_H: 'H'
+}
 
-var figure = {};
+var Ranks = {
+    Rank_1: 1,
+    Rank_2: 2,
+    Rank_3: 3,
+    Rank_4: 4,
+    Rank_5: 5,
+    Rank_6: 6,
+    Rank_7: 7,
+    Rank_8: 8
+}
+
+var Colors = {
+    White: 1,
+    Black: 0
+}
+
+var Board = {
+    cells: new Array(),
+}
+
+var Figure = {
+    name: '',
+    color: '',
+    move: genValidMove()
+};
 
 function bindEventHandlers() {
     var cells = $('.board div div');
