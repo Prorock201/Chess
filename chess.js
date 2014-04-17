3c685c3fedd7aa68df8cb21f98f1adc3a22b12ae
Prorock201 <prorock@list.ru>
2014-04-17 22:35:44 +0300

    1. "reBuild"

--- chess.js
+++ chess.js
@@ -3,15 +3,22 @@
  */
 window.onload = bindEventHandlers;
 
+var Desk2 = [];
+
+var Desk = [
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    [8, 7, 6, 5, 4, 3, 2, 1]
+];
+
 var Files = {
-    File_A: 'A',
-    File_B: 'B',
-    File_C: 'C',
-    File_D: 'D',
-    File_E: 'E',
-    File_F: 'F',
-    File_G: 'G',
-    File_H: 'H'
+    File_A: 'a',
+    File_B: 'b',
+    File_C: 'c',
+    File_D: 'd',
+    File_E: 'e',
+    File_F: 'f',
+    File_G: 'g',
+    File_H: 'h'
 }
 
 var Ranks = {
@@ -37,21 +44,87 @@ var Board = {
 var Figure = {
     name: '',
     color: '',
-    move: genValidMove()
+    move: ''
 };
+
+var piecePosition = '';
+var pieceAbb = '';
+var pieceRank = '';
+var pieceFile = '';
+var pieceRankIndex = '';
+var pieceFileIndex = '';
+var MoveTo = [];
 
 function bindEventHandlers() {
     var cells = $('.board div div');
     cells.click(function (event) {
         var square = event.currentTarget;
         if ($(event.currentTarget).children()[0]) {
-            console.log($(square).children()[0]);
+            piecePosition = $(event.currentTarget).attr('class');
             $(cells).each(function (index, element) {
                 if ($(element).hasClass('pickUp')) {
                     $(element).removeClass('pickUp')
                 }
                 $(square).addClass('pickUp');
             });
+            var pieceImg = $(event.currentTarget).children()[0];
+            pieceAbb = $(pieceImg).attr('src').substr(8,1);
+            separatePieceAbb (piecePosition);
+            console.log(piecePosition);
+            console.log(pieceAbb);
+        }
+
+        switch (pieceAbb) {
+            case 'b':
+                checkBishopMove();
+                break;
+            case 'n':
+                checkKnightMove();
+                break
+            case 'r':
+                checkRockMove();
+                break;
+            case 'q':
+                checkQueenMove();
+                break;
+            case 'k':
+                checkKingMove();
+                break;
+            case 'p':
+                checkPawnMove();
+                break;
         }
     });
+
+    $('.board div div').each(function (index, element) {
+        Desk2.push($(element).attr('class'));
+    });
+
+    console.log(Desk2);
+}
+
+function separatePieceAbb (piecePosition) {
+    pieceRank = piecePosition.split('')[0];
+    pieceFile = piecePosition.split('')[1];
+}
+
+function checkKnightMove() {
+    $(Desk[0]).each(function (index, element) {
+        if (element == pieceRank) {
+            pieceRankIndex = index;
+        }
+    });
+
+    $(Desk[1]).each(function (index2, element2) {
+        if (element2 == pieceFile) {
+            pieceFileIndex = index2;
+        }
+    });
+
+    console.log(Desk[0][index - 1]);
+    console.log(Desk[1][index - 2]);
+
+    /*MoveTo.push()*/
+
+
 }
